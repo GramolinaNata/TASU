ee08efd80e3434bf11e1ce5150766b8b3d5688f2
GramolinaNata <gramolina.natali@mail.ru>
2026-04-21 23:43:08 +0500

    feat: batch routes and controller

---
+++ migrate_railway.js
@@ -0,0 +1,7 @@
+﻿const {PrismaClient} = require('@prisma/client');
+process.env.DATABASE_URL = 'postgresql://postgres:cmupgaSaRsSsSurrSNvtiLXibQDUYqJX@shinkansen.proxy.rlwy.net:42194/railway';
+const p = new PrismaClient();
+p.$executeRaw`SELECT 1`.then(() => {
+  console.log('connected');
+  return p.$disconnect();
+});
---
+++ push_railway.js
@@ -0,0 +1,3 @@
+﻿process.env.DATABASE_URL = 'postgresql://postgres:cmupgaSaRsSsSurrSNvtiLXibQDUYqJX@shinkansen.proxy.rlwy.net:42194/railway';
+const {execSync} = require('child_process');
+execSync('npx prisma db push', {stdio: 'inherit', env: {...process.env}});
